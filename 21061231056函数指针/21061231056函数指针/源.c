ee407da5cd435e76a1d7fe87020cfb8492cc6422
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2022-05-28 09:21:34 +0800

    Add files via upload

---
+++ 21061231056函数指针/21061231056函数指针/源.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+//int add(int x,int y)
+//{
+//	return x + y;
+//}
+//
+//int main()
+//{
+// 	int (*pa)(int,int)= add;
+//	int ret= (*pa)(2, 3);
+//	printf("%d", ret);
+//	return 0;
+//}
+
+//int add(int x,int y)
+//{
+//	return x + y;
+//}
+//int sub(int x, int y)
+//{
+//	return x - y;
+//}
+//int mul(int x, int y)
+//{
+//	return x * y;
+//}
+//int div(int x, int y)
+//{
+//	return x / y;
+//}
+//int main()//函数指针数组
+//{
+//	 int (*p)(int,int) = add;
+//	 int (*pa[4])(int, int) = {&add,&sub,&mul,&div};
+//	 int i = 0;
+//	 for (i = 0; i < 4; i++)
+//	 {
+//		 printf("%d ",pa[i](2,3));
+//	 }
+//	 return 0;
+//}
+
