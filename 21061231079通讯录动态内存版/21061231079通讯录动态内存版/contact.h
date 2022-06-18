96bc2e4b7a464e8be88ccab6f8324580d85bf20a
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2022-06-19 06:30:47 +0800

    Add files via upload

---
+++ 21061231079通讯录动态内存版/21061231079通讯录动态内存版/contact.h
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#define max_name 10
+#define max_sex 10
+#define max_tele 12
+#define max_addr 20
+#define DEFAULT_sz 3
+//#define max 100
+enum input
+{
+	e,
+	add,
+	del,
+	search,
+	modify,
+	show,
+	sort
+};
+struct peoinfo//描述人的信息
+{
+	char name[max_name];
+	int age;
+	char sex[max_sex];
+	char tele[max_tele];
+	char addr[max_addr];
+};
+struct contact//通讯录类型，加个size方便计数
+{
+	struct peoinfo* data;
+	int size;
+	int capacity;//记录通讯录容量
+};
+//函数声明
+void initcontact(struct contact* ps);//初始化通讯录
+//增加人
+void addcontact(struct contact* ps);
+//删除人
+void delcontact(struct contact* ps);
+//打印信息
+void showcontact(const struct contact* ps);
+//查询
+void searchcontact(const struct contact* ps);
+//修改信息
+void modifycontact(struct contact* ps);
+//销毁通讯录
+void destorycontact(struct contact* ps);
+
+
