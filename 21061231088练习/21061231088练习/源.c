f902483b9aef5a9a28657ca889532437ee8a024a
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2022-06-29 12:21:09 +0800

    Add files via upload

---
+++ 21061231088练习/21061231088练习/源.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+
+//void func(int i)
+//{
+//	i = 20;
+//}
+//
+//int main()
+//{
+//	int i = 10;
+//	func(i);
+//	printf("%d",i);
+//	return 0;
+//}
+
+//void Func(int b[])
+//{
+//	int j = 0;
+//	for (j = 0; j < 4; j++)
+//	{
+//		b[j] = j;
+//	}
+//}
+//
+//int main()
+//{
+//	static int a[] = { 5,6,7,8 }, i;
+//	Func(a);
+//	for (i = 0; i < 4; i++)
+//	{
+//		printf("%d ",a[i]);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int  arr[7][7] = { 0 }, i = 0, j = 0;
+//
+//	for (i = 0; i < 7; i++)
+//	{
+//		for (j = 0; j <= i ;j++)
+//		{
+//			arr[i][0] = 1;
+//			arr[i][i] = 1;
+//			if (j<i&&j!=0)//处理再1内部
+//			{
+//				arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+//			}
+//			printf("%d  ",arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+
+
+
+
+
