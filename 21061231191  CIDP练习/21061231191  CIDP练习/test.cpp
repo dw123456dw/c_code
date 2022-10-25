c7b5ea909a117284e33dda8c5c6554a608e5ca0d
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2022-10-25 16:04:10 +0800

    Add files via upload

---
+++ 21061231191  CIDP练习/21061231191  CIDP练习/test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+
+void test1()
+{
+	int score = 0;
+	cin >> score;
+
+	cout << "gong xi ni kao le " << score << " fen!" << endl;
+}
+
+
+void test2()
+{
+
+
+
+}
+
+
+
+int main()
+{
+	//test1();
+	test2();
+	return 0;
+}
