5600271d8125b1c232abb0083e5975fef071e036
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2022-12-13 12:42:25 +0800

    Add files via upload

---
+++ 21061231212      test   12_12/21061231212      test   12_12/源.cpp
@@ -0,0 +1,60 @@
+﻿//#include <iostream>
+//using namespace std;
+//#include <string>
+//int main()
+//{
+//    // 请在此输入您的代码
+//    //今天一看就是个哈希表嘛
+//    string s;
+//    cin >> s;
+//    int hash[26] = { 0 };
+//
+//    for (int i = 0; i < s.size(); i++)
+//    {
+//        hash[s[i] - 'a']++;
+//    }
+//    int max = 0;
+//    int pos = 0;
+//
+//    for (int i = 0; i < 26; i++)
+//    {
+//        if (hash[i] > max)
+//        {
+//            max = hash[i];
+//            pos = i;
+//        }
+//    }
+//
+//    cout << (char)(pos+'a') << endl;
+//    cout << max << endl;
+//    return 0;
+//}
+
+
+#include <iostream>
+using namespace std;
+#include <string>
+int main()
+{
+    // 请在此输入您的代码
+    string s;
+    int num;
+    cin >> s >> num;
+
+    while (num--)
+    {
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] > s[i + 1])
+            {
+                s.erase(i, 1);
+                break;
+            }
+        }
+
+    }
+
+
+    cout << s;
+    return 0;
+}
