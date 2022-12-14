367e34750ccb0ec2eeda06d99b3e606d9378238d
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2022-12-14 11:19:00 +0800

    Add files via upload

---
+++ 21061231213   蓝桥真题/21061231213   蓝桥真题/test.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    // 请在此输入您的代码
+    //一眼哈希表统计
+    int hash[10] = { 0 };
+
+    //2 12 20 21 22
+    for (int i = 1; i <= 22; i++)
+    {
+        int temp = i;
+        while (temp)
+        {
+            int a = temp % 10;
+            hash[a]++;
+            temp /= 10;
+        }
+    }
+
+    cout << hash[2] << endl;
+    return 0;
+}
