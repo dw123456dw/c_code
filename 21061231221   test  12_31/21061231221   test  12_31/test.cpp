44c2b1110c1247f9cd105b20c6dfa4c5261fcb7a
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-01-05 15:05:31 +0800

    Add files via upload

---
+++ 21061231221   test  12_31/21061231221   test  12_31/test.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+int arr[30][30];
+#include <vector>
+vector<int> ret;
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+	for (int i = 1; i <=1; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (arr[i][j] == 1)
+			{
+				ret.push_back(j);
+			}
+		}
+	}
+	for (int i = 0; i < ret.size(); i++)
+	{
+		cout << ret[i] << " ";
+	}
+	return 0;
+}
