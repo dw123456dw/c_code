19a981cf9796604eb860c7b5d0a8071cb53ff272
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-01-06 09:32:39 +0800

    Add files via upload

---
+++ 21061231222  test   1_1/21061231222  test   1_1/222.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+char repeatedCharacter(string s) {
+    int hash[30] = {0};
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        hash[s[i] - 'a']++;
+        for (int j=0;j<26;j++)
+        {
+            if (hash[j]==2)
+            {
+                return (char)(j+'a');
+            }
+        }
+    }
+}
+
+
+
+int main()
+{
+    cout << repeatedCharacter("abccbaacz");
+
+
+
+    return 0;
+}
