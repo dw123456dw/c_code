25217b37591ca87dc36397898117d677a1df1b64
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-01-11 12:17:51 +0800

    Add files via upload

---
+++ 21061231227  test  1_9/21061231227  test  1_9/源.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+using namespace std;
+#include <vector>
+
+int reinitializePermutation(int n) {
+    vector<int> arr(n);
+    vector<int> temp(n);
+    vector<int> a(n);
+    int ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = i;
+        a[i] = i;
+    }
+
+    while (!equal(arr.begin(), arr.end(), temp.begin(), temp.end()))
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (j % 2 == 0)
+            {
+                temp[j] = a[j / 2];
+            }
+            if (j % 2 == 1)
+            {
+                temp[j] = a[n / 2 + (j - 1) / 2];
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = temp[i];
+        }
+
+        ans++;
+    }
+    return ans;
+}
+
+
+int main()
+{
+    cout << reinitializePermutation(20);
+
+
+    return 0;
+}
