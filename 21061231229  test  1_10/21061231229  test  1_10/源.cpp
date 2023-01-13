e65d8d08e5bac075968da1b7b1274550c9db351e
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-01-13 13:56:52 +0800

    Add files via upload

---
+++ 21061231229  test  1_10/21061231229  test  1_10/源.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+#include <vector>
+
+vector<vector<int>> ans;
+vector<int> temp;
+void backtracking(int n, int k, int startindex)
+{
+    if (temp.size() == k)
+    {
+        ans.push_back(temp);
+        return;
+    }
+    //单层循环
+    for (int i = startindex; i <= n; i++)
+    {
+        temp.push_back(i);
+        backtracking(n, k, i + 1);
+        temp.pop_back();
+    }
+}
+vector<vector<int>> combine(int n, int k) {
+    backtracking(n, k, 1);
+    return ans;
+}
+
+int main()
+{
+    combine(4,2);
+    return 0;
+}
