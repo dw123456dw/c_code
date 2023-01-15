384d2cfdc76b16c2d72b1869d5f895b971303716
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-01-15 12:11:14 +0800

    Add files via upload

---
+++ 21061231231  test  1_13/21061231331  test  1_13/源.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+
+
+int rearrangeCharacters(string s, string target) {
+    int hash[26];
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        hash[s[i] - 'a']++;
+    }
+
+    int pos = 0, ans = 0;
+
+    while (hash[target[pos] - 'a'] != 0)
+    {
+        while (pos < target.size())
+        {
+            if (hash[target[pos] - 'a'] == 0)
+            {
+                break;
+            }
+            hash[target[pos++] - 'a']--;
+        }
+        pos = 0;
+        ans++;
+    }
+    return ans;
+}
+
+
+int main()
+{
+    string s = "ilovecodingonleetcode";
+    string a = "code";
+    int ans = rearrangeCharacters(s,a);
+    cout << ans;
+
+    return 0;
+}
