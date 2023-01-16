5875033caa843ee0a095668f55cb9efe0ab147f9
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-01-16 14:03:33 +0800

    Add files via upload

---
+++ 21061231232  test 1_15/21061231232  test 1_15/源.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+#include <vector>
+
+int minMaxGame(vector<int>& nums) {
+    int n = nums.size();
+    if (n == 1)
+    {
+        return nums[0];
+    }
+
+    while (nums.size() != 1)
+    {
+        vector<int> arr(nums.size()/2);
+        for (int i = 0; i < nums.size()/2; i++)
+        {
+            if (i % 2 == 0)
+            {
+                arr[i] = min(nums[2 * i], nums[2 * i + 1]);
+            }
+            else
+            {
+                arr[i] = max(nums[2 * i], nums[2 * i + 1]);
+            }
+        }
+        nums.clear();
+        for (int i = 0; i < arr.size(); i++)
+        {
+            nums.push_back(arr[i]);
+        }
+    }
+    return nums[0];
+}
+
+int main()
+{
+    vector<int> a = { 1,3,5,2,4,8,2,2 };
+    cout << minMaxGame(a);
+
+    return 0;
+}
