8301faa46417e2dc953cedd4dc0b36a194457ab7
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-02-12 21:51:08 +0800

    Add files via upload

---
+++ 21061231234  test 1_21/2106231234  test 1_21/源.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+#include <vector>
+#include <unordered_map>
+
+int numSubarraysWithSum(vector<int>& nums, int goal) {
+    int sum = 0;
+    unordered_map<int, int> cnt;
+    int ret = 0;
+    for (auto& num : nums) {
+        cnt[sum]++;
+        sum += num;
+        ret += cnt[sum - goal];
+    }
+    return ret;
+}
+
+
+int main()
+{
+    vector<int> a = {1,0,1,0,1};
+    cout << numSubarraysWithSum(a,2);
+
+    return 0;
+}
