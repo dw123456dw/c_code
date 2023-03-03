04a087f6f9a63242203b3037700d31d5a51cc50c
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-03-03 22:12:03 +0800

    Add files via upload

---
+++ 21061231238  test 3_3/21061231238  test 3_3/test.c
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+int main()
+{
+	printf("hello world\n");
+
+//#pragma omp parallel
+//	{
+//		printf("hai\n");
+//	}
+//
+//	printf("hello openmp");
+	int tid, mcpu;
+
+	tid = omp_get_thread_num();
+	mcpu = omp_get_num_threads();
+
+	printf("Hello from thread %d in %d CPUs\n",tid,mcpu);
+	printf("---------before parallel\n");
+	printf("\n");
+	printf("---------during parallel\n");
+
+	#pragma omp parallel num_threads(3) private(tid,mcpu)
+	{
+		tid = omp_get_thread_num();
+		mcpu = omp_get_num_threads();
+		printf("Hello from thread %d in %d CPUs\n", tid, mcpu);
+	}
+
+
+	printf("\n");
+	printf("--------after parallel\n");
+	printf("Hello from thread %d in %d CPUs\n", tid, mcpu);
+
+	return 0;
+}
