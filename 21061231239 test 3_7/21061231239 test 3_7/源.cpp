9fa87eca22e1ecb33b469a20b46ee76ad3131146
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-03-07 21:58:00 +0800

    Add files via upload

---
+++ 21061231239 test 3_7/21061231239 test 3_7/源.cpp
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <omp.h>
+
+
+
+int main()
+{
+	int tid, i, j, a[10], b[10];
+	omp_set_num_threads(3);
+
+	for (i=0;i<4;i++)
+	{
+		a[i] = -10;
+		b[i] = -10;
+	}
+
+	tid = omp_get_thread_num();
+	for (i=0;i<4;i++)
+	{
+		printf("a[%d] = %d  ",i,a[i]);
+	}
+	printf("thread %d\n",tid);
+
+
+	printf("----------------\n");
+
+
+#pragma omp parallel for private(i,j,tid),firstprivate(a,b),lastprivate(b)
+	for (i=0;i<4;i++)
+	{
+		tid = omp_get_thread_num();
+		printf("a[%d]=%d  b[%d]=%d  thread %d\n",i,a[i],i,b[i],tid);
+
+		a[i] = (i + 1) * 3 + tid;
+		b[i] = (i + 1) * 7 + tid;
+
+		printf("a[%d]=%d  b[%d]=%d  thread %d  changed\n", i, a[i], i, b[i], tid);
+	}
+
+	return 0;
+}
