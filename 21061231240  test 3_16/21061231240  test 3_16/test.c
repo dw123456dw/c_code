86ab1537b93256090a406f4491d57ed9c58432f4
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-03-16 21:18:39 +0800

    Add files via upload

---
+++ 21061231240  test 3_16/21061231240  test 3_16/test.c
@@ -0,0 +1,29 @@
+﻿#include<stdio.h>
+#include<omp.h>
+int x;
+#pragma omp threadprivate(x)
+intmain() {
+	int tid;
+	x = 10;
+	omp_set_num_threads(3);
+	#pragma omp parallel
+	{
+		tid = omp_get_thread_num();
+		printf("tid=%d,x=%d\n",tid,x);
+		x = x + 1;
+		printf("tid=%d,x=%dchenged\n",tid,x);
+	}
+	printf("afterparallel1st");
+	printf("x=%d\n", x);
+	printf("-----2nd-----\n");
+	omp_set_num_threads(3);
+	#pragma omp parallel
+	{
+		tid = omp_get_thread_num();
+		printf("tid=%d,x=%d",tid,x);
+		x = x + 1;
+		printf("tid=%d,x=%dchanged\n",tid,x);
+	}
+	printf("afterparallel2nd\n");
+	printf("x=%d\n", x);
+}
