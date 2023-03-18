781d08957aabdbe0dc5473d2579e3c7b06d74d44
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-03-18 19:32:28 +0800

    Add files via upload

---
+++ 21061231241  test  3_17/21061231241  test  3_17/源.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+
+
+vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+    sort(nums.begin(), nums.end());
+    int i = 0;
+
+    vector<int> ans(queries.size());
+
+    for (int j = 0; j < queries.size(); j++)
+    {
+        int sum = 0;
+        int count = 0;
+        while (sum < queries[j] && i < nums.size())
+        {
+            count++;
+            sum += nums[i++];
+        }
+        i = 0;
+        ans[j] = count;
+    }
+
+    return ans;
+}
+
+
+int main()
+{
+    vector<int> a1 = { 4,5,2,1 };
+    vector<int> a2 = {3,10,21};
+    vector<int> a3 = answerQueries(a1,a2);
+
+
+    return 0;
+}
