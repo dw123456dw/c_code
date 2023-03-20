aad99cce7bec591f8a7c85ec3b66128efc6e310f
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-03-20 16:20:49 +0800

    Add files via upload

---
+++ 21061231242  蓝桥练习/21061231242  蓝桥练习/源.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+#include <vector>
+
+void fun(vector<vector<int>> arr,int n,int m)
+{
+	vector<vector<int>> copyarr(n, vector<int>(n));
+	vector<vector<int>> output(n, vector<int>(n));
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			int a;
+			cin >> a;
+			copyarr[i][j] = a;
+		}
+	}
+
+	for (int i=0;i<n;i++)
+	{
+
+	}
+}
+
+int main()
+{
+	int m, n;
+	cin >> n >> m ;
+
+	vector<vector<int>> temp(n,vector<int>(n));
+
+	for (int i=0;i<n;i++)
+	{
+		for (int j=0;j<n;j++)
+		{
+			int a;
+			cin >> a;
+			temp[i][j] = a;
+		}
+	}
+	return 0;
+}
+
+
+
