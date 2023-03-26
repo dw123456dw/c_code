038c81499d8a24f35299f963fd5a42fe29ac908f
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-03-26 11:04:49 +0800

    Add files via upload

---
+++ 21061231243   test  3_24/21061231243   test  3_24/test.c
@@ -0,0 +1,37 @@
+﻿#include <stdio.h>
+#include <omp.h>
+
+
+//int main()
+//{
+//	int nthreads_set, nthreads, tid;
+//	nthreads_set = 80;
+//
+//	omp_set_dynamic(1);
+//	omp_set_num_threads(nthreads_set);
+//	printf("set_number_threads=%d\n",nthreads_set);
+//	printf("dynamic region (1 or 0): %d\n",omp_get_dynamic());
+//	printf("\n");
+//
+//#pragma omp parallel private (tid,nthreads)
+//	{
+//		nthreads = omp_get_num_threads();
+//		tid = omp_get_thread_num();
+//		printf("number of threads=%d  ",nthreads);
+//		printf("tid = %d\n",tid);
+//		printf("----------------\n");
+//	}
+//
+//	return 0;
+//}
+
+int main()
+{
+	int n = 10;
+
+	double sum = 0.0;
+	int x = 1;
+
+
+	return 0;
+}
