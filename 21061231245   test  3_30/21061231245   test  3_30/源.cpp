a1fc27c7c5e1cbae21babe55b7f3f3ec90e5a15a
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-03-30 20:37:11 +0800

    Add files via upload

---
+++ 21061231245   test  3_30/21061231245   test  3_30/源.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+#include <unordered_set>
+
+int lengthOfLongestSubstring(string s) {
+    if (s.size() == 0) return 0;
+    unordered_set<char> hash;
+    int ans = 0;
+    int left = 0;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        while (hash.find(s[i]) != hash.end())
+        {
+            hash.erase(s[left]);
+            left++;
+        }
+        ans = max(ans, i - left + 1);
+        hash.insert(s[i]);
+    }
+    return ans;
+}
+int main()
+{
+    string s = "abcabcbb";
+    cout << lengthOfLongestSubstring(s);
+   
+    return 0;
+}
