3b95229c01fa4d2053323ec59cb5f4a2549cc390
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-04-01 21:32:53 +0800

    Add files via upload

---
+++ 21061231247  test  4_1/21061231247  test  4_1/源.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+#include <vector>
+#include <algorithm>
+int main()
+{
+    // 请在此输入您的代码
+    vector<int> arr;
+
+    int n;
+    cin >> n;
+
+    int pos = 0;
+
+    while (scanf_s("%d", &arr[pos++]) != EOF)
+    {
+        ;
+    }
+
+    sort(arr.begin(), arr.end());
+
+    int a = 0, b = 0;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        if (arr[i] - arr[i - 1] == 2)
+        {
+            a = arr[i] + 1;
+        }
+
+        if (arr[i] == arr[i - 1])
+        {
+            b = arr[i];
+        }
+    }
+    cout << a << " " << b;
+    return 0;
+}
