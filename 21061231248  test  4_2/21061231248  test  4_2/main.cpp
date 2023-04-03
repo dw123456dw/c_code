966e91d790e89bb333ed5927229795cfef915b12
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-04-03 12:09:23 +0800

    Add files via upload

---
+++ 21061231248  test  4_2/21061231248  test  4_2/main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+#include <string>
+#include <stdio.h>
+int main()
+{
+	// 请在此输入您的代码
+	string month[13] = { "","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec" };
+
+	int m, d;
+	cin >> m >> d;
+	cout << month[m];
+	printf("%02d", d);
+	return 0;
+}
