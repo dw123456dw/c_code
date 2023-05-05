9a6cf4198e0fdfa30b06fd1fbac7475e58567705
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-05-05 12:55:05 +0800

    Add files via upload

---
+++ 21061231259  test   4_26/21061231259  test   4_26/FileName.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	for (int i=0;i<t;i++)
+	{
+		int n;
+		cin >> n;
+
+		for (int m=0;m<=n-1;m++)
+		{
+
+		}
+
+
+	}
+
+
+
+	return 0;
+}
