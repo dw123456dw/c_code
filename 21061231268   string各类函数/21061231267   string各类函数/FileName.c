05bbf9ecc51a28464192949149e4b40ee8daed0e
dw123456dw <98172022+dw123456dw@users.noreply.github.com>
2023-06-09 19:03:38 +0800

    Add files via upload

---
+++ 21061231268   string各类函数/21061231267   string各类函数/FileName.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool stringcompare(char* str1[], char* str2[])
+{
+	int len1 = sizeof(str1);
+	int len2 = sizeof(str2);
+
+	if (len1!=len2)
+	{
+		return false;
+	}
+	char* s1 = str1;
+	char* s2 = str2;
+	while (*s1!='\0')
+	{
+		if (*s1!=*s2)
+		{
+			return false;
+		}
+		s1++;
+		s2++;
+	}
+	return true;
+}
+
+int main()
+{	
+	if (stringcompare("abc","abc"))
+	{
+		printf("相同\n");
+	}
+	else
+	{
+		printf("不一样\n");
+	}
+	return 0;
+}
